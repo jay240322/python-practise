d3ea9ed9740a6e9fd53487009814a9fee81a18fd
jay patel <patel.jay.brij03@gmail.com>
2025-06-22 18:52:40 +0530

    done

---
+++ ch4/ch4(one).py
@@ -0,0 +1,12 @@
+# Write a program to store seven fruits in a list entered by the user.
+fruit = []
+first = input("enter the firts fruit name:")
+fruit.append(first)
+second = input("enter the firts fruit name:")
+fruit.append(second)
+third = input("enter the firts fruit name:")
+fruit.append(third)
+fourth = input("enter the firts fruit name:")
+fruit.append(fourth)
+
+print(fruit)
