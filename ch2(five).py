f74a0a36f372c3bbcd971eb00c5e57bfc02a264a
jay patel <patel.jay.brij03@gmail.com>
2025-06-22 18:20:25 +0530

    done

---
+++ ch2(five).py
@@ -0,0 +1,5 @@
+# Write a python program to find an average of two numbers entered by the user
+
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+print("the average of two number is :",(a+b)/2)
