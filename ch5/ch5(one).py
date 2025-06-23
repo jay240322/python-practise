6b7fda2f71eb8982cb4bd0e92291b2fae602aaef
jay patel <patel.jay.brij03@gmail.com>
2025-06-23 09:28:06 +0530

    done

---
+++ ch5/ch5(one).py
@@ -0,0 +1,11 @@
+# Write a program to create a dictionary of Hindi words with values as their English
+# translation. Provide user with an option to look it up!
+words = {
+    "help":"madad",
+    "chair":"khursi",
+    "cat":"billi",
+    "dog":"kuta"
+}
+
+word = input("enter the word :")
+print(words[word])
