c1d2ce689f74b140966e69cc89434a46f6c76159
jay patel <patel.jay.brij03@gmail.com>
2025-06-23 09:08:56 +0530

    done

---
+++ ch4/ch4(four).py
@@ -0,0 +1,13 @@
+#  Write a program to sum a list with 4 numbers.
+list = []
+a = int(input("enter the first number :"))
+list.append(a)
+b = int(input("enter the second number :"))
+list.append(b)
+c = int(input("enter the third number :"))
+list.append(c)
+d = int(input("enter the four number :"))
+list.append(d)
+
+
+print(sum(list))
