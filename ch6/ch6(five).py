79807b052fdefe28a5925f657aa4d9cf7260b48a
jay patel <patel.jay.brij03@gmail.com>
2025-06-23 17:45:55 +0530

    done

---
+++ ch6/ch6(five).py
@@ -0,0 +1,9 @@
+# Write a program which finds out whether a given name is present in a list or not.
+
+name = ["jay","sumit","shubham","meet","paklo"]
+
+username = input("enter the name :")
+if(username in name):
+    print("alreadey taken")
+else:
+    print("done")
---
+++ ch6/ch6(seven).py
@@ -0,0 +1,7 @@
+# Write a program to find out whether a given post is talking about “Harry” or not
+
+
+username = input("enter the username:")
+
+if("jay" in username.lower()):
+    print("use this is JAY")
