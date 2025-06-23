6565fb9500bfc38b9af4ae094ca963e00b501115
jay patel <patel.jay.brij03@gmail.com>
2025-06-23 09:53:11 +0530

    done

---
+++ ch5/ch5(seven).py
@@ -0,0 +1,18 @@
+#  If the names of 2 friends are same; what will happen to the program in problem
+# 6?
+d = {}
+name = input("enter the name:")
+language = input("enter the language name:")
+d.update({name:language})
+name = input("enter the name:")
+language = input("enter the language name:")
+d.update({name:language})
+name = input("enter the name:")
+language = input("enter the language name:")
+d.update({name:language})
+name = input("enter the name:")
+language = input("enter the language name:")
+d.update({name:language})
+print(d)
+
+# it will change update the subject name
