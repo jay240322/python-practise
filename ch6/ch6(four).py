9771e26163988a3ca0662273f763463cffd11580
jay patel <patel.jay.brij03@gmail.com>
2025-06-23 17:25:58 +0530

    done

---
+++ ch6/ch6(four).py
@@ -0,0 +1,9 @@
+# Write a program to find whether a given username contains less than 10
+# characters or not.
+
+username = input("enter the username:")
+
+if(len(username)<10):
+    print("the username letter is to short")
+else:
+    print("Done")
