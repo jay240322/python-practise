5a3f89eb75b9d84e88a7451f1f282eb0b45a64a7
jay patel <patel.jay.brij03@gmail.com>
2025-06-25 09:04:32 +0530

    done

--- ch7/ch7(nine).py
+++ ch7/ch7(nine).py
@@ -3,3 +3,9 @@
 # *   *       for n = 3
 # * * *
 
+number = int(input("enter the number:"))
+for i in range(1, number + 1):
+    if i == 1 or i == number:
+        print("* " * number)
+    else:
+        print("*" + " " * (2 * (number - 2) + 1) + "*")
